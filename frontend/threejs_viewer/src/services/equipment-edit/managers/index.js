738102cc2cc81b219c399745dbaf414d5a4268c4
Dongjoon Lee <drjoony@gmail.com>
2026-01-25 19:54:30 +0900

    Sidebar refactoring

---
+++ frontend/threejs_viewer/src/services/equipment-edit/managers/index.js
@@ -0,0 +1,9 @@
+/**
+ * managers/index.js - Barrel Export
+ * @version 1.0.0
+ * 📁 위치: frontend/threejs_viewer/src/services/equipment-edit/managers/index.js
+ */
+
+export { MappingDataManager } from './MappingDataManager.js';
+export { PersistenceManager } from './PersistenceManager.js';
+export { AutoSaveManager } from './AutoSaveManager.js';
---
+++ frontend/threejs_viewer/src/services/equipment-edit/utils/imdex.js
@@ -0,0 +1,9 @@
+/**
+ * utils/index.js - Barrel Export
+ * @version 1.0.0
+ * 📁 위치: frontend/threejs_viewer/src/services/equipment-edit/utils/index.js
+ */
+
+export { StatisticsCalculator } from './StatisticsCalculator.js';
+export { ImportExportManager } from './ImportExportManager.js';
+export { DebugUtils, registerGlobalDebugFunctions } from './DebugUtils.js';
